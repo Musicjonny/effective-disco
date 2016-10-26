da8b1ee3c1c35ff60505ec860ee23c6e7eef9700
Johannes <josija94@gmail.com>
2016-10-26 10:36:14 +0200

    first gyro

--- gyro/gyro.cpp
+++ gyro/gyro.cpp
@@ -1,26 +1,19 @@
-#include "gyro.h"
-
-void setup_gyro(){
-  Serial.begin(9600);
-  L3G gyro;
-  Wire.begin();
-  L3G gyro;
-  if (!gyro.init())
-  {
-    Serial.println("Failed to autodetect gyro type!");
-    while (1);
-  }
-
-  gyro.enableDefault();
-}
-
-void read_rotation(){
-  Serial.println((int)gyro.g.z);
-  delay(100);
-}
-
-int add_rotation(){
-  rotation += (int)gyro.g.z;
-  return rotation;
-}
+//#include "gyro.h"
+//
+//void setup_gyro(){
+//  Serial.begin(9600);
+//  
+//  Wire.begin();
+//  
+//}
+//
+//void read_rotation(L3G gyro){
+//  Serial.println((int)gyro.g.z);
+//  delay(100);
+//}
+//
+//int add_rotation(L3G gyro){
+//  rotation += (int)gyro.g.z;
+//  return rotation;
+//}
 
--- gyro/gyro.ino
+++ gyro/gyro.ino
@@ -1,29 +1,84 @@
-#include "gyro.h"
+/*
+The sensor outputs provided by the library are the raw 16-bit values
+obtained by concatenating the 8-bit high and low gyro data registers.
+They can be converted to units of dps (degrees per second) using the
+conversion factors specified in the datasheet for your particular
+device and full scale setting (gain).
 
-#define LED1 
-#define LED2 
+Example: An L3GD20H gives a gyro X axis reading of 345 with its
+default full scale setting of +/- 245 dps. The So specification
+in the L3GD20H datasheet (page 10) states a conversion factor of 8.75
+mdps/LSB (least significant bit) at this FS setting, so the raw
+reading of 345 corresponds to 345 * 8.75 = 3020 mdps = 3.02 dps.
+*/
 
-int actual_rotation = 0;
+#include <Wire.h>
+#include <L3G.h>
+
+L3G gyro;
+long drehung = 0;
+long richtung = 0;
+static int LED1 = 12;
+static int LED2 = 11;
 
 void setup() {
-  setup_gyro();
+  Serial.begin(9600);
+  Wire.begin();
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
 
+  if (!gyro.init())
+  {
+    Serial.println("Failed to autodetect gyro type!");
+    while (1);
+  }
+
+  gyro.enableDefault();
 }
 
+long i = 0;
+long sum = 0;
+
 void loop() {
-  read_rotation();
-  actual_rotation = add_rotation();
-  if (actual_rotation > 0)
-  {
+  gyro.read();
+
+//  Serial.print(" Aktuelle Drehung: ");
+  drehung = (int)gyro.g.z-230;
+  
+/* Mittelwert
+  while(i < 100000){
+    sum += drehung;
+    i++;
+    //if (i % 1000 == 0)
+   //   Serial.println(i);
+  }
+  Serial.println((float)sum/100000);
+  sum = 0;
+  i = 0;
+ */
+
+  if (drehung > -70 && drehung < 70) 
+    drehung = 0;
+  drehung = drehung/100;
+  Serial.println(drehung);
+ // Serial.print(", Drehrichtung: ");
+  richtung += drehung;
+  if (richtung > 1000){
     digitalWrite(LED1, HIGH);
     digitalWrite(LED2, LOW);
   }
-  else if (actual_rotation < 0)
-  {
+ //   Serial.println("Rechts");
+  else if (richtung < -1000){
     digitalWrite(LED2, HIGH);
     digitalWrite(LED1, LOW);
   }
-
+  
+  
+//    Serial.println("Links");
+  else{
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED1, LOW);
+  }
+ //   Serial.println("gerade");
+  Serial.println(richtung);
 }
